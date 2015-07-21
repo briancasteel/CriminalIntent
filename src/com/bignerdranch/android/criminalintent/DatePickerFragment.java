1bce38c3ce2544e6e8914155c7ef8615193e35cb
Brian Casteel <bcasteel@alumni.utexas.net>
2015-07-20 19:42:46 -0500

    Update from home

--- src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 
 /**
  * Created by Brian Casteel on 7/16/2015.
@@ -12,7 +13,11 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = getActivity().getLayoutInflater()
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
