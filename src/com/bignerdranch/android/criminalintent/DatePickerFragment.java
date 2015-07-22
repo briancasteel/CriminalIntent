0b37052df61f90bb1c9c08b68ef438a4db99b3ab
Brian Casteel <bcasteel@alumni.utexas.net>
2015-07-21 22:19:50 -0500

    Adding date extraction and initialization

--- src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -5,7 +5,11 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by Brian Casteel on 7/16/2015.
@@ -28,9 +32,26 @@ public class DatePickerFragment extends DialogFragment {
         mDate = (Date)getArguments().getSerializable(EXTRA_DATE);
 
         // Create a Calendar to get the year, month, and day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         View v = getActivity().getLayoutInflater()
                 .inflate(R.layout.dialog_date, null);
+
+        DatePicker datePicker = (DatePicker)v.findViewById(R.id.dialog_date_datePicker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // Translate year, month, day into a Date object using a calendar
+                mDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+
+                // Update argument to preserve selected value on rotation
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
