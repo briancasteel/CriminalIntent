5c3733916f5b9e4fc6ef3aaae889de81f4c1f43b
Brian Casteel <bcasteel@alumni.utexas.net>
2015-11-09 06:51:49 -0600

    na

--- src/com/bignerdranch/android/criminalintent/Crime.java
+++ src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -9,6 +12,11 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+
+    private static final String JSON_ID = "id";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_SOLVED = "solved";
+    private static final String JSON_DATE = "date";
 
     public Date getDate() {
         return mDate;
@@ -49,5 +57,14 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, mId.toString());
+        json.put(JSON_TITLE, mTitle);
+        json.put(JSON_SOLVED, mSolved);
+        json.put(JSON_DATE, mDate.getTime());
+        return json;
     }
 }
